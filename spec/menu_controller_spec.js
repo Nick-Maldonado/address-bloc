3dd580282f20ce85bcafe891d3a5f8ab789504ec
Nick-Maldonado <nickmaldonado94@gmail.com>
2018-07-26 17:39:27 -0500

    tests written

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -1,10 +1,12 @@
 const MenuController = require("../controllers/MenuController");
 
 describe("MenuController", () => {
+  
   describe("#getContactCount()", () => {
     beforeEach(() => {
       this.menu = new MenuController();
     })
+
     it("should return 0 when no contacts are in the book", () => {
       expect(this.menu.getContactCount()).toBe(0);
     });
@@ -12,7 +14,21 @@ describe("MenuController", () => {
     it("should return 1 when there is exactly one contact in the book", () => {
       this.menu.contacts.push("Bob");
       expect(this.menu.getContactCount()).toBe(1);
-    })
+    });
   });
-});
 
+  describe("#remindMe()", () => {
+    beforeEach(() => {
+      this.menu = new MenuController();
+    });
+
+    it("should return 'Learning is a life-long pursuit.' when called", () => {
+      expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit.");
+    });
+
+    it("should not return anything other than 'Learning is a life-long pursuit.", () => {
+      expect(this.menu.remindMe() !== "Learning is a life-long pursuit.").toBeFalsy()
+    });
+  })
+
+});
